dd7b9edf969a4dbe063828f178a5fea599c04556
Rakhman22 <rahman.sabirov.02@mail.ru>
2022-11-16 13:27:58 +0600

    Raha234

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ password = input("Введите пароль: ")
 if password == "sshh":
     print("Добро пожаловать")
 elif password == "qwert":
-    print("Hello!")
+    print("Hello! world")
 else:
     print("Доступ запрещен")
 #Adilet
+#Changed to world by Rakhman
